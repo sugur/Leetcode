b174d48ffd27bc97033058a47afadd4f23627eac
ssu-wei <s14725836s12@gmail.com>
2019-08-18 16:52:05 +0800

    add trees

--- src/main/java/idv/swtang/leetcode/tree/SumOfLeftLeaves.java
+++ src/main/java/idv/swtang/leetcode/tree/SumOfLeftLeaves.java
@@ -1,5 +1,34 @@
 package idv.swtang.leetcode.tree;
 
+import idv.swtang.leetcode.common.TreeNode;
+
+//404. Sum of Left Leaves
+//Find the sum of all left leaves in a given binary tree.
+//
+//Example:
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
 public class SumOfLeftLeaves {
 
+	
+	
+	
+	 public int sumOfLeftLeaves(TreeNode root) {
+	        if (root == null) return 0;
+	        return sumOfLeftLeavesHelper(root, false);
+	    }
+
+	    private int sumOfLeftLeavesHelper(TreeNode root, boolean leftFlag) {
+	        if (root == null) return 0;
+	        int left = sumOfLeftLeavesHelper(root.left, true);
+	        int right = sumOfLeftLeavesHelper(root.right, false);
+	        return left + right + (leftFlag && root.left == null && root.right == null ? root.val : 0);
+	    }
 }
